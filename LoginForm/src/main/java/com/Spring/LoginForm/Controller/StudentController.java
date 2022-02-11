9c14d92e10e4fc7cc443dfe1063eac0ddd7533db
naveenprabakarank <83070579+naveenprabakarank@users.noreply.github.com>
2022-02-11 15:33:25 +0530

    Update StudentController.java

--- LoginForm/src/main/java/com/Spring/LoginForm/Controller/StudentController.java
+++ LoginForm/src/main/java/com/Spring/LoginForm/Controller/StudentController.java
@@ -24,7 +24,7 @@ public class StudentController {
 		String error=" ";
 		try{
 	    // declaration and instantiation of objects/variables  
-	    System.setProperty("webdriver.chrome.driver", "LoginForm\\src\\main\\resources\\chromedriver.exe");  
+	    System.setProperty("webdriver.chrome.driver", "C:\\Downloads\\chromedriver.exe");  
 	    WebDriver driver=new ChromeDriver();  
 
 	    // Launch website  
