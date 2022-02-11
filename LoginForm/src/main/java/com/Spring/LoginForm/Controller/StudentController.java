551cf9dae72f5456a2ca6c4c9a65884b29dd9aa6
naveenprabakarank <83070579+naveenprabakarank@users.noreply.github.com>
2022-02-11 15:17:47 +0530

    Update StudentController.java

--- LoginForm/src/main/java/com/Spring/LoginForm/Controller/StudentController.java
+++ LoginForm/src/main/java/com/Spring/LoginForm/Controller/StudentController.java
@@ -1,5 +1,7 @@
 package com.Spring.LoginForm.Controller;
-
+import org.openqa.selenium.By;  
+import org.openqa.selenium.WebDriver;  
+import org.openqa.selenium.chrome.ChromeDriver;  
 import java.io.IOException;
 import java.io.*;
 import org.springframework.web.bind.annotation.ModelAttribute;
@@ -19,25 +21,19 @@ public class StudentController {
 
 	@RequestMapping("/home")
 	public String Student(){
-		String error=" ";
-		Runtime rt = Runtime.getRuntime();
-    try {
-        rt.exec(new String[]{"cmd.exe","/c","start"});
+	    // declaration and instantiation of objects/variables  
+	    System.setProperty("webdriver.chrome.driver", "LoginForm/src/main/resources/chromedriver.exe");  
+	    WebDriver driver=new ChromeDriver();  
 
-    } catch (IOException e) {
-		
-		StringWriter sw = new StringWriter();
- 
-            // create a PrintWriter
-            PrintWriter pw = new PrintWriter(sw);
-            e.printStackTrace(pw);
- 
-            error = sw.toString();
- 
-		
-		
+	    // Launch website  
+	    driver.navigate().to("http://www.google.com/");  
+
+	    // Click on the search text box and send value  
+	    driver.findElement(By.id("lst-ib")).sendKeys("javatpoint tutorials");  
+
+	    // Click on the search button  
+	    driver.findElement(By.name("btnK")).click();  
+	    return "sucess";
 	}
-		return error;
-}
 	
 }
