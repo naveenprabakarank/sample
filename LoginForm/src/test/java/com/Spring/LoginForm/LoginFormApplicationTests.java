cebc71e0556024684c75c1d12bbf4aa36eb63fc5
naveenprabakarank <83070579+naveenprabakarank@users.noreply.github.com>
2022-04-12 12:45:37 +0530

    Update LoginFormApplicationTests.java

--- LoginForm/src/test/java/com/Spring/LoginForm/LoginFormApplicationTests.java
+++ LoginForm/src/test/java/com/Spring/LoginForm/LoginFormApplicationTests.java
@@ -2,12 +2,28 @@ package com.Spring.LoginForm;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 @SpringBootTest
 class LoginFormApplicationTests {
 
 	@Test
 	void contextLoads() {
+		 WebDriverManager.chromedriver().setup();
+		    
+
+		  
+	        WebDriver driver = new ChromeDriver();
+
+	    
+
+	        driver.get("https://google.com");
+	        String title = driver.getTitle();
+	        System.out.print(title);
+			
+	        driver.quit();
 	}
 
 }
