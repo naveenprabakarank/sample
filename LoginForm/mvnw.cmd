ca71f022c39c649b3bb2a78ab3750cdad35f39fc
naveenprabakarank <83070579+naveenprabakarank@users.noreply.github.com>
2025-11-16 12:03:39 +0530

    Update mvnw.cmd

--- LoginForm/mvnw.cmd
+++ LoginForm/mvnw.cmd
@@ -40,7 +40,7 @@ title %0
 @REM enable echoing by setting MAVEN_BATCH_ECHO to 'on'
 @if "%MAVEN_BATCH_ECHO%" == "on"  echo %MAVEN_BATCH_ECHO%
 
-@REM set %HOME% to equivalent of $HOME
+@REM set %HOME% to equivalent of $HOME 
 if "%HOME%" == "" (set "HOME=%HOMEDRIVE%%HOMEPATH%")
 
 @REM Execute a user defined script before this one
